18b5b73fe8de5e0926d94728f642d5e780f89dba
kitkat <kit@example.com>
2019-12-08 22:34:27 +0000

    Basic fault multiplication

--- faulting_multiplications/operation.c
+++ faulting_multiplications/operation.c
@@ -13,17 +13,12 @@
 #include <unistd.h>
 #include <x86intrin.h>
 
-int verbose=1, display_calculation=0;
+int verbose=1;
 int fd;
 int faulty_result_found=0;
 int undervolting_finished=0;
 uint64_t plane0_zero;
 uint64_t plane2_zero;
-int printing_position_y=6;
-int printing_position_x=1;
-char printme[100];
-WINDOW *mainwin;
-
 
 typedef struct calcuation_info_t
 {
@@ -72,7 +67,7 @@ void reset_voltage()
 	voltage_change(fd,plane0_zero);
 	voltage_change(fd,plane2_zero);
 	if (verbose)
-		printf("\nVoltage reset\n");
+		printf("\r\nVoltage reset\r\n");
 }
 
 void *undervolt_it(void *input)
@@ -101,7 +96,7 @@ void *undervolt_it(void *input)
 	&& ( ui->voltage > ui->end_undervoltage ||  ui->stop_after_x_drops>counter))
 	{
 		if (verbose)
-			printf("\r\nUndervolting: %i\r\n",ui->voltage);
+			printf("Undervolting: %i\n",ui->voltage);
 		counter++;
 		
 		voltage_change(fd,wrmsr_value(ui->voltage,0));
@@ -118,39 +113,6 @@ void *undervolt_it(void *input)
 	return NULL;
 }
 
-void screen_init()
-{
-	if ((mainwin=initscr()) == NULL)
-	{
-		fprintf(stderr, "Error initialising ncurses.\n");
-		exit (EXIT_FAILURE);
-	}	
-	start_color();
-	init_pair(1, COLOR_GREEN,  COLOR_BLACK);
-	init_pair(2, COLOR_BLACK, COLOR_GREEN);
-	init_pair(3, COLOR_BLACK, COLOR_RED);
-	init_pair(4, COLOR_WHITE, COLOR_RED);
-	color_set(1,NULL);
-}
-
-void screen_del()
-{
-	delwin(mainwin);
-	endwin();
-	refresh();
-}
-
-void print_calculation(uint64_t one, uint64_t two, int thread_number)
-{
-	curs_set(0);
-	color_set(2,NULL);
-	sprintf(printme, " ---- core %i ---- ",thread_number);
-	mvaddstr( 2 + (thread_number*3),printing_position_x+10,printme);
-	color_set(1,NULL);
-	sprintf(printme, "0x%08lx * 0x%08lx = 0x%016lx    ",one, two, one*two);
-	mvaddstr( 3 + (thread_number*3),printing_position_x,printme);
-	refresh();
-}
 
 void *multiply_it(void *input)
 {
@@ -162,7 +124,6 @@ void *multiply_it(void *input)
 	uint64_t min2=ci->operand2_min;
 	unsigned int one,two;
 	
-
 	while (faulty_result_found==0 && undervolting_finished==0)
 	{
 		ci->iterations_performed=0;
@@ -170,16 +131,12 @@ void *multiply_it(void *input)
 		{
 			__builtin_ia32_rdrand32_step(&one);
 			ci->operand1=one % (max1 - min1) + min1;
-
 		}
 		if (ci->max_or_fixed_op2=='M')
 		{
 			__builtin_ia32_rdrand32_step(&two);
 			ci->operand2=(two % (max2-min2)) + 1+min2 ;
 		}
-		/* SHOW THE CALCULATION ON THE SCREEN */
-		if (display_calculation)
-			print_calculation(ci->operand1,ci->operand2,ci->thread_number);
 		do
 		{
 			ci->iterations_performed++;
@@ -195,7 +152,6 @@ void *multiply_it(void *input)
 			faulty_result_found=1;
 		}
 	}
-
 	return NULL;
 }
 
@@ -217,7 +173,6 @@ void usage(const char* program)
 	printf("\t -w #           \t operand 2 minimum - default=0\n");
 	printf("\t -S             \t Silent mode - default=verbose\n");
 	printf("\t -M             \t Multiply only (no undervolting)\n");
-	printf("\t -C             \t display Calculation \n");
 	printf("\t -h             \t display this Help\n");
 
 }
@@ -312,9 +267,6 @@ int main (int argc, char* argv[])
 	    case 'M':
 		multiply_only=1;
 		break;
-	    case 'C':
-		display_calculation=1;
-		break;
             case ':':  
                 printf("option needs a value\n");  
                 break;  
@@ -346,31 +298,31 @@ int main (int argc, char* argv[])
 		printf ("Error: operand1 and operand2 must be set.\n");
 		return -1;
 	}
-      
 	if (verbose)
 	{
-		printf("\nSummary\n");
-		printf("-------------------------------------------------\n");
-		printf("Iterations:          %li\n",iterations);
-		printf("Start Voltage:       %li\n",start_undervoltage);
-		printf("End Voltage:         %li\n",end_undervoltage);
-		printf("Stop after x drops:  %i\n",stop_after_x_drops);
-		printf("Voltage steps:       %i\n",voltage_steps);
-		printf("Threads:             %i\n",number_of_threads);
-		printf("Operand1:            0x%016lx\n",operand1);
-		printf("Operand2:            0x%016lx\n",operand2);
-		printf("Operand1 is:         %s\n",max_or_fixed_op1=='M'?"maximum":"fixed value");
-		printf("Operand2 is:         %s\n",max_or_fixed_op2=='M'?"maximum":"fixed value");
-		printf("Operand1 min is:     0x%016lx\n",operand1_min);
-		printf("Operand2 min is:     0x%016lx\n",operand2_min);
-		printf("Multiply only:	     %s\n",multiply_only==1?"Yes":"No");
-		printf("Display calculation: %s\n",display_calculation==1?"Yes":"No");
+		printf("\r\nSummary\n");
+		printf("\r-------------------------------------------------\n");
+		printf("\rIterations:          %li\n",iterations);
+		printf("\rStart Voltage:       %li\n",start_undervoltage);
+		printf("\rEnd Voltage:         %li\n",end_undervoltage);
+		printf("\rStop after x drops:  %i\n",stop_after_x_drops);
+		printf("\rVoltage steps:       %i\n",voltage_steps);
+		printf("\rThreads:             %i\n",number_of_threads);
+		printf("\rOperand1:            0x%016lx\n",operand1);
+		printf("\rOperand2:            0x%016lx\n",operand2);
+		printf("\rOperand1 is:         %s\n",max_or_fixed_op1=='M'?"maximum":"fixed value");
+		printf("\rOperand2 is:         %s\n",max_or_fixed_op2=='M'?"maximum":"fixed value");
+		printf("\rOperand1 min is:     0x%016lx\n",operand1_min);
+		printf("\rOperand2 min is:     0x%016lx\n",operand2_min);
+		printf("\rMultiply only:	     %s\n",multiply_only==1?"Yes":"No");
 	}
 	plane0_zero = wrmsr_value(0,0);
 	plane2_zero = wrmsr_value(0,2);
 
 	pthread_t* my_calculation_thread=malloc(sizeof(pthread_t)*number_of_threads);
 	calculation_info* calculation_data=malloc(sizeof(calculation_info)*number_of_threads);
+
+
 	for (int i=0;i<number_of_threads;i++)
 	{
 		calculation_data[i].operand1=operand1;
@@ -392,7 +344,6 @@ int main (int argc, char* argv[])
 	undervoltage_data.stop_after_x_drops=stop_after_x_drops;
 	undervoltage_data.voltage_steps=voltage_steps;
 
-	screen_init();
 	if (multiply_only==0)
 	{
 		pthread_t my_undervolting_thread;
@@ -404,58 +355,39 @@ int main (int argc, char* argv[])
 	{
     		pthread_join(my_calculation_thread[i], NULL);
 	}
-    	
 
 	for (int i=0;i<number_of_threads;i++)
 	{
 		if (calculation_data[i].correct_a!=calculation_data[i].correct_b)
 		{
-			clear();
-			printing_position_y=3;
 			uint64_t correct=calculation_data[i].operand1*calculation_data[i].operand2;
-			color_set(4,NULL);
-			
-			mvaddstr(printing_position_y++,0,"------   CALCULATION ERROR DETECTED   ------");
-			color_set(1,NULL);
-			sprintf(printme," > Iterations  \t : %08li\n"  , calculation_data[i].iterations_performed);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
-			sprintf(printme," > Operand 1   \t : %016lx\n" , calculation_data[i].operand1);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
-			sprintf(printme," > Operand 2   \t : %016lx\n" , calculation_data[i].operand2);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
-			sprintf(printme," > Correct     \t : %016lx\n" , calculation_data[i].operand1*calculation_data[i].operand2);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
+			printf("\n------   CALCULATION ERROR DETECTED   ------\n");
+			printf(" > Iterations  \t : %08li\n"  , calculation_data[i].iterations_performed);
+			printf(" > Operand 1   \t : %016lx\n" , calculation_data[i].operand1);
+			printf(" > Operand 2   \t : %016lx\n" , calculation_data[i].operand2);
+			printf(" > Correct     \t : %016lx\n" , calculation_data[i].operand1*calculation_data[i].operand2);
 			if (correct!=calculation_data[i].correct_a)
-				sprintf(printme," > Result      \t : %016lx\n" , calculation_data[i].correct_a);
+				printf( " > Result      \t : %016lx\n" , calculation_data[i].correct_a);
 			if (correct!=calculation_data[i].correct_b)
-				sprintf(printme," > Result       \t : %016lx\n" , calculation_data[i].correct_b);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
-			sprintf(printme," > xor result  \t : %016lx\n" , calculation_data[i].correct_a^calculation_data[i].correct_b);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
-			sprintf(printme," > undervoltage\t : %i\n"     , undervoltage_data.voltage+1);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
+				printf( " > Result      \t : %016lx\n" , calculation_data[i].correct_b);
+			printf( " > xor result  \t : %016lx\n" , calculation_data[i].correct_a^calculation_data[i].correct_b);
+			printf( " > undervoltage\t : %i\n"     , undervoltage_data.voltage+1);
 			char* cmd1="sensors | grep \"Core 0\" |awk '{print $3}'i | tr -d '\\n'";
 			FILE *fp=popen(cmd1,"r");
 			char temp[10];
 			fgets(temp,10,fp);
 			fclose(fp);
-			sprintf(printme," > temperature \t : %s\n",temp);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
+			printf( " > temperature \t : %s\n",temp);
 			char* cmd2="cat /proc/cpuinfo | grep MHz | head -n 1 | cut  -d ':' -f2| cut -d '.' -f1 | tr -d '\\n'";
 			fp=popen(cmd2,"r");
 			char freq[10];
 			fgets(freq,10,fp);
-			sprintf(printme," > Frequency   \t : %sMHz\n",freq);
-			mvaddstr(printing_position_y++,printing_position_x,printme);
+			printf( " > Frequency   \t :%sMHz\n",freq);
 			refresh();
 			fclose(fp);	
-			mvaddstr(21,1," [Done] Press any key.");
-			getch();
-			screen_del();
 		}
 	}
-    	delwin(mainwin);
-   	endwin();
-	printf("[Done]\n");
-	return 0;
+	printf("Done.\n");
+	getch();
+        return 0;
 }
